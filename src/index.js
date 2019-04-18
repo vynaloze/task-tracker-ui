c3f2d25a53b8ca58c49e90cc401e9286e179904f
Piotr Pawluk <vynaloze@gmail.com>
2019-04-18 09:31:16 +0200

    Basic projects view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Project(props) {
+    return (
+        <div>{props.value.name}</div>
+    );
+}
+
+class Projects extends React.Component {
+    render() {
+        const projectsJson = [
+            {
+                "name": "string",
+                "id": 1
+            },
+            {
+                "name": "222",
+                "id": 2
+            }
+        ];
+
+        const projects = projectsJson.map((p) => (
+            <div>
+                <Project value={p}/>
+            </div>
+        ));
+
+        return (
+            <div className='projects'>
+                {projects}
+            </div>
+
+        )
+    }
+}
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <Projects/>
+        );
+    }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById('root')
+);
